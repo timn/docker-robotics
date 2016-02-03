48b45a0768e7b877583c8401cda6c107861245fe
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2016-02-03 09:53:32 +0100

    fawkes-robotino: image to run fawkes-robotino in RCLL simulation
    This docker image is based on fedora-ros:23-jade and adds the Fawkes
    domain release for the RoboCup Logistics League (RCLL), and the refbox
    and simulation models, plugins, and worlds for the RCLL.
    
    The image can be run with just
      docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix \
        -e DISPLAY=unix$DISPLAY --name rcll-sim --privileged \
        timn/fawkes-robotino:2015-f23-jade
    
    It will bring up the full simulation. Confer
    https://trac.fawkesrobotics.org/wiki/Carologistics/Gazsim-Setup-2015#RunningaGame
    on how to actually run the game from there.

---
+++ fawkes-robotino/rcll-run-sim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Preparing simulation environment"
+
+source /etc/profile
+
+export FAWKES_DIR=/opt/fawkes-robotino
+export GAZEBO_RCLL=/opt/gazebo-rcll
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$GAZEBO_RCLL/plugins/lib/gazebo
+export GAZEBO_MODEL_PATH=$GAZEBO_RCLL/models
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$GAZEBO_RCLL/models/carologistics
+
+export LLSF_REFBOX_DIR=/opt/llsf-refbox
+export GAZEBO_WORLD_PATH=/opt/gazebo-rcll/worlds/carologistics/llsf.world
+
+mkdir -p /run/dbus
+dbus-daemon --system --fork
+
+echo "Running simulation"
+cd $FAWKES_DIR/bin
+./gazsim.bash -x start -n 1 -r -a
+
+echo "Startup complete. Press Enter to shutdown."
+read
